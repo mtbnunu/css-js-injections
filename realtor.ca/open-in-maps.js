833c39194d734a239dfb74974d81470766950b5c
Jae Yeong Bae <mtbnunu@gmail.com>
2020-02-26 20:20:18 -0800

    Update open-in-maps.js

--- realtor.ca/open-in-maps.js
+++ realtor.ca/open-in-maps.js
@@ -10,14 +10,19 @@ $(document).ready(()=>{
         listingAddress.parentNode.insertBefore(linkFrag, listingAddress.nextSibling);
     }
 
-    //add details to modal
-    const price = $("#listingPrice").text();
-    const bb = $("#BedroomIcon .listingIconNum").text() + "/" + $("#BathroomIcon .listingIconNum").text();
-    const space = $("#propertyDetailsSectionVal_InteriorFloorSpace .propertyDetailsSectionContentValue").text();
-    $(".m_ryl_sld_hdr_address")[0].append("|  " + price + " | " + bb + " | " + space)
+	//add details to modal
+	const price = $("#listingPrice").text();
+	const bb = $("#BedroomIcon .listingIconNum").text() + "/" + $("#BathroomIcon .listingIconNum").text();
+	const space = $("#propertyDetailsSectionVal_InteriorFloorSpace .propertyDetailsSectionContentValue").text();
+	$(".m_ryl_sld_hdr_address")[0].append("|  " + price + " | " + bb + " | " + space)
 
-    //link to sol
-    const link = "https://www.searchonlinelistings.ca/listing?key=" + $("#MLNumberVal").text();
-    const atag = "<a href='"+link +"' target='_blnk'>Open in SOL</a>"
-    $("#listingDetailsTabsCon").prepend(atag)
+	//link to other sites
+	const mls = $("#MLNumberVal").val();
+
+	const sollink = "https://www.searchonlinelistings.ca/listing?key=" + mls
+	const rewlink = "https://www.rew.ca/properties/search/results?query=" + mls
+
+	const place = $("#listingDetailsTabsCon");
+	place.prepend("<a href='"+sollink +"'>SOL | </a>")
+	place.prepend("<a href='"+rewlink +"'>REW | </a>")
 })
