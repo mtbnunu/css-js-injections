a1b2adf4267981befee24ec9ea60d914d52657da
Jae Yeong Bae <mtbnunu@gmail.com>
2020-02-26 22:36:30 -0800

    Update open-in-maps.js

--- realtor.ca/open-in-maps.js
+++ realtor.ca/open-in-maps.js
@@ -1,14 +1,35 @@
 $(document).ready(()=>{
-    const listingAddress = window.document.getElementById("listingAddress");
-    if(listingAddress){
-        const address = listingAddress.innerText.replace("\n"," ");
-        const link = "https://www.google.com/maps?t=k&q=" + encodeURIComponent(address);
-        const linkFrag = window.document.createElement("a");
-        linkFrag.href = link;
-        linkFrag.target = "_blank";
-        linkFrag.text = "View on Google Maps"
-        listingAddress.parentNode.insertBefore(linkFrag, listingAddress.nextSibling);
-    }
+	const listingAddress = window.document.getElementById("listingAddress");
+	if(listingAddress){
+	
+	    // add maps link
+	    const address = listingAddress.innerText.replace("\n"," ");
+	    const link = "https://www.google.com/maps?t=k&q=" + encodeURIComponent(address);
+	    const linkFrag = window.document.createElement("a");
+	    linkFrag.href = link;
+	    linkFrag.target = "_blank";
+	    linkFrag.text = "View on Google Maps"
+	    listingAddress.parentNode.insertBefore(linkFrag, listingAddress.nextSibling);
+
+
+	    // add bc assessment link
+	    const streetaddr = listingAddress.innerText.split("\n")[0]
+	    fetch("https://cors-anywhere.herokuapp.com/"+"https://www.bcassessment.ca/Property/Search/GetByAddress?addr="+encodeURIComponent(streetaddr), 
+	    {
+		headers:{
+		    'X-Requested-With': 'XMLHttpRequest'
+		}
+	    })
+	    .then((response) => response.json())
+	    .then((data) => {
+		console.log(data)
+		if(data.length === 1){
+			const bcassessmentlink = "https://www.bcassessment.ca//Property/Info/" + data[0].value;
+		    $(listingAddress).after("<a href='"+bcassessmentlink+"' target='_blank'>BCAssessment</a><br>")
+		}
+	    });
+
+	}
 
 	//add details to modal
 	const price = $("#listingPrice").text();
@@ -16,7 +37,7 @@ $(document).ready(()=>{
 	const space = $("#propertyDetailsSectionVal_InteriorFloorSpace .propertyDetailsSectionContentValue").text();
 	$(".m_ryl_sld_hdr_address")[0].append("|  " + price + " | " + bb + " | " + space)
 
-	//link to other sites
+	//link to sol
 	const mls = $("#MLNumberVal").text();
 
 	const sollink = "https://www.searchonlinelistings.ca/listing?key=" + mls
