4ef257f7c4a4bdbeae3dbf730ba5444d465bd90d
Jae Yeong Bae <mtbnunu@gmail.com>
2020-02-26 19:33:19 -0800

    Update open-in-maps.js

--- realtor.ca/open-in-maps.js
+++ realtor.ca/open-in-maps.js
@@ -1,13 +1,18 @@
 $(document).ready(()=>{
-    const listingAddress = window.document.getElementById("listingAddress");
-    if(listingAddress){
-        const address = listingAddress.innerText.replace("\n"," ");
-        const link = "https://www.google.com/maps?t=k&q=" + encodeURIComponent(address);
-        const linkFrag = window.document.createElement("a");
-        linkFrag.href = link;
-        linkFrag.target = "_blank";
-        linkFrag.text = "View on Google Maps"
-        listingAddress.parentNode.insertBefore(linkFrag, listingAddress.nextSibling);
-    }
-    
-    })
+const listingAddress = window.document.getElementById("listingAddress");
+if(listingAddress){
+    const address = listingAddress.innerText.replace("\n"," ");
+    const link = "https://www.google.com/maps?t=k&q=" + encodeURIComponent(address);
+    const linkFrag = window.document.createElement("a");
+    linkFrag.href = link;
+    linkFrag.target = "_blank";
+    linkFrag.text = "View on Google Maps"
+    listingAddress.parentNode.insertBefore(linkFrag, listingAddress.nextSibling);
+}
+
+//add details to modal
+const price = $("#listingPrice").text();
+const bb = $("#BedroomIcon .listingIconNum").text() + "/" + $("#BathroomIcon .listingIconNum").text();
+const space = $("#propertyDetailsSectionVal_InteriorFloorSpace .propertyDetailsSectionContentValue").text();
+$(".m_ryl_sld_hdr_address")[0].append("|  " + price + " | " + bb + " | " + space)
+})
