b0574d56118fc593126fd1a75e0a2bec4d20fafb
Jae Yeong Bae <mtbnunu@gmail.com>
2020-06-05 16:46:40 -0700

    Update index.js

--- searchonlinelistings.ca/index.js
+++ searchonlinelistings.ca/index.js
@@ -3,7 +3,7 @@ $(document).ready(()=>{
 	const rltlink = "https://www.realtor.ca/map#ReferenceNumber=" + mls
 	const rewlink = "https://www.rew.ca/properties/search/results?query=" + mls
 
-	const place = 	$(".info-container")
+	const place = 	$(".detail-info")
 	place.prepend("<a href='"+rltlink +"'>Realtor.ca | </a>")
 	place.prepend("<a href='"+rewlink +"'>REW | </a>")
 });
