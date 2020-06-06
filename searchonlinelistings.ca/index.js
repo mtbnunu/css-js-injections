10f5991d4008d1235204cb0aa1835e29bab3b213
Jae Yeong Bae <mtbnunu@gmail.com>
2020-06-05 17:00:29 -0700

    Update index.js

--- searchonlinelistings.ca/index.js
+++ searchonlinelistings.ca/index.js
@@ -1,3 +1,16 @@
+(function() {
+    function l(u, i) {
+        var d = document;
+        if (!d.getElementById(i)) {
+            var s = d.createElement('script');
+            s.src = u;
+            s.id = i;
+            d.body.appendChild(s);
+        }
+    }
+    l('//code.jquery.com/jquery-3.2.1.min.js', 'jquery')
+})();
+
 $(document).ready(()=>{
 	function contains(selector, text) {
 	  var elements = document.querySelectorAll(selector);
