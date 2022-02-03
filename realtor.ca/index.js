a8dc44e125397f0d5e2cb391027ca0355b19954e
Jae Yeong Bae <mtbnunu@gmail.com>
2022-02-02 20:02:30 -0800

    fix bcassessment

--- realtor.ca/index.js
+++ realtor.ca/index.js
@@ -1,20 +1,30 @@
 $(document).ready(()=>{
 	const listingAddress = window.document.getElementById("listingAddress");
+	
+	const addLink = (link, text)=>{
+		console.log(link, text)
+		const linkDivFrag = window.document.createElement("div");
+		
+	    const linkFrag = window.document.createElement("a");
+	    linkFrag.href = link;
+	    linkFrag.target = "_blank";
+	    linkFrag.text = text
+	    
+	    linkDivFrag.appendChild(linkFrag)
+	    
+	    listingAddress.parentNode.insertBefore(linkDivFrag, listingAddress.nextSibling);
+	}
+	
 	if(listingAddress){
 	
 	    // add maps link
 	    const address = listingAddress.innerText.replace("\n"," ");
-	    const link = "https://www.google.com/maps?t=k&q=" + encodeURIComponent(address);
-	    const linkFrag = window.document.createElement("a");
-	    linkFrag.href = link;
-	    linkFrag.target = "_blank";
-	    linkFrag.text = "View on Google Maps"
-	    listingAddress.parentNode.insertBefore(linkFrag, listingAddress.nextSibling);
+	    addLink("https://www.google.com/maps?t=k&q=" + encodeURIComponent(address), "View on Google Maps")
 
 
 	    // add bc assessment link
 	    const streetaddr = listingAddress.innerText.split("\n")[0]
-	    fetch("https://cors-anywhere.herokuapp.com/"+"https://www.bcassessment.ca/Property/Search/GetByAddress?addr="+encodeURIComponent(streetaddr), 
+	    fetch("https://api.allorigins.win/get?url="+"https://www.bcassessment.ca/Property/Search/GetByAddress?addr="+encodeURIComponent(streetaddr), 
 	    {
 		headers:{
 		    'X-Requested-With': 'XMLHttpRequest'
@@ -23,9 +33,10 @@ $(document).ready(()=>{
 	    .then((response) => response.json())
 	    .then((data) => {
 		console.log(data)
-		if(data.length === 1){
-			const bcassessmentlink = "https://www.bcassessment.ca//Property/Info/" + data[0].value;
-		    $(listingAddress).after("<a href='"+bcassessmentlink+"' target='_blank'>BCAssessment</a><br>")
+		const jsonData = JSON.parse(data.contents)
+		if(jsonData.length === 1){
+			const bcassessmentlink = "https://www.bcassessment.ca//Property/Info/" + jsonData[0].value;
+		    addLink(bcassessmentlink, "View on BCAssessment")
 		}
 	    });
 
@@ -40,10 +51,10 @@ $(document).ready(()=>{
 	//link to sol
 	const mls = $("#MLNumberVal").text();
 
-	const sollink = "https://www.searchonlinelistings.ca/listing?key=" + mls
-	const rewlink = "https://www.rew.ca/properties/search/results?query=" + mls
+	// const sollink = "https://www.searchonlinelistings.ca/listing?key=" + mls
+	// const rewlink = "https://www.rew.ca/properties/search/results?query=" + mls
 
-	const place = $("#listingDetailsTabsCon");
-	place.prepend("<a href='"+sollink +"'>SOL | </a>")
-	place.prepend("<a href='"+rewlink +"'>REW | </a>")
+	// const place = $("#listingDetailsTabsCon");
+	// place.prepend("<a href='"+sollink +"'>SOL | </a>")
+	// place.prepend("<a href='"+rewlink +"'>REW | </a>")
 })
